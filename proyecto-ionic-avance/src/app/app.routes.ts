bc939ee7c22641ba0903e17c875a3cc6cf661f1f
Eric <eecalero@sudamericano.edu.ec>
2026-02-19 21:36:49 -0500

    Delete proyecto-ionic-avance directory

--- proyecto-ionic-avance/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePage } from './home/home.page';
-import { RegistroPage } from './registro/registro.page';
-
-export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'registro', component: RegistroPage }
-];
