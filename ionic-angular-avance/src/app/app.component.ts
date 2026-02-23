d0ed59119a0a4d07bbfe6a8c906d974c0b6f5baf
Eric <eecalero@sudamericano.edu.ec>
2026-02-22 20:05:36 -0500

    Delete ionic-angular-avance directory

--- ionic-angular-avance/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<ion-app><router-outlet></router-outlet></ion-app>'
-})
-export class AppComponent {}
